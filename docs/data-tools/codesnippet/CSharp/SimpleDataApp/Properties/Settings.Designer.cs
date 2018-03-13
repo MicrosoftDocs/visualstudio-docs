50e7109766dd7ae6722ccba94d7bc098c5909668
Genevieve Warren <gewarren@microsoft.com>
2018-03-13 12:00:19 -0700

    fixed discrepancy between connString and connStr; fix #684

--- docs/data-tools/codesnippet/CSharp/SimpleDataApp/Properties/Settings.Designer.cs
+++ docs/data-tools/codesnippet/CSharp/SimpleDataApp/Properties/Settings.Designer.cs
@@ -9,29 +9,29 @@
 //------------------------------------------------------------------------------
 
 namespace SimpleDataApp.Properties {
-    
-    
+
+
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
-        
+
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-        
+
         public static Settings Default {
             get {
                 return defaultInstance;
             }
         }
-        
+
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
         [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(localdb)\\ProjectsV13;Initial Catalog=Sales;Integrated Security=True;" +
             "Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=R" +
             "eadWrite;MultiSubnetFailover=False")]
-        public string connStr {
+        public string connString {
             get {
-                return ((string)(this["connStr"]));
+                return ((string)(this["connString"]));
             }
         }
     }
