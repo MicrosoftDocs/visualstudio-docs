68d928230dcf8d22ad82b503124566e614718aa3
Nick Schonning <nschonni@gmail.com>
2018-10-08 14:53:04 -0400

    typo: Double word "a"

--- docs/vs-2015/snippets/visualbasic/VS_Snippets_VSSDK/vssdkcompletiontest/vb/snippetutilities.vb
+++ docs/vs-2015/snippets/visualbasic/VS_Snippets_VSSDK/vssdkcompletiontest/vb/snippetutilities.vb
@@ -78,7 +78,7 @@ Namespace ImplementingCodeSnippetsPart1
 
             'check for a commit character
             If nCmdID = CUInt(VSConstants.VSStd2KCmdID.RETURN) OrElse nCmdID = CUInt(VSConstants.VSStd2KCmdID.TAB) OrElse (Char.IsWhiteSpace(typedChar) OrElse Char.IsPunctuation(typedChar)) Then
-                'check for a a selection
+                'check for a selection
                 If m_session IsNot Nothing AndAlso (Not m_session.IsDismissed) Then
                     'if the selection is fully selected, commit the current session
                     If m_session.SelectedCompletionSet.SelectionStatus.IsSelected Then
@@ -263,7 +263,7 @@ Namespace ImplementingCodeSnippetsPart2
 
             'check for a commit character
             If nCmdID = CUInt(VSConstants.VSStd2KCmdID.RETURN) OrElse nCmdID = CUInt(VSConstants.VSStd2KCmdID.TAB) OrElse (Char.IsWhiteSpace(typedChar) OrElse Char.IsPunctuation(typedChar)) Then
-                'check for a a selection
+                'check for a selection
                 If m_session IsNot Nothing AndAlso (Not m_session.IsDismissed) Then
                     'if the selection is fully selected, commit the current session
                     If m_session.SelectedCompletionSet.SelectionStatus.IsSelected Then
